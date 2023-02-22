83833843bdf9159da3f702bf23a32a43f05aaaf8
mizuki <sma0729mex@gmail.com>
2023-02-23 02:12:55 +0900

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by Apple on 2023/02/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
