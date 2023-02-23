de121ceb3ad55f90edb278c36ba3a82b77ecedb7
mizuki <sma0729mex@gmail.com>
2023-02-23 09:48:05 +0900

    commit

--- c++/main.cpp
+++ c++/main.cpp
@@ -10,6 +10,6 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    cout << "Hello, World!!!!!z!" << endl;
+    cout << "Hello, World!!!!!z" << endl;
     return 0;
 }
